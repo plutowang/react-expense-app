1bc038a646fbb00924cc47aa06efa631762a1e88
plutowang <pluto.wbc@gmail.com>
2020-03-04 21:49:34 -0500

    feat(router): add private router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,27 +1,25 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
-// import { createBrowserHistory } from 'history';
 import AddExpensePage from "../components/AddExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
 import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
-import Header from "../components/Header";
 import HelpPage from "../components/HelpPage";
 import NotFoundPage from "../components/NotFoundPage";
 import LoginPage from "../components/LoginPage";
+import PrivateRoute from "./PrivateRoute";
 
 export const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact={true} />
-        <Route path="/dashboard" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+        <PrivateRoute path="/help" component={HelpPage} />
 
         <Route component={NotFoundPage} />
       </Switch>
