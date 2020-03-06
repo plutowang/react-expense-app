8590c57776edf47b1e37932b048b4cbef5b62f26
plutowang <pluto.wbc@gmail.com>
2020-03-06 00:03:31 -0500

    perf(babel): add babel polyfill to support older browser

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,7 @@ module.exports = env => {
   const MiniCssExtract = new MiniCssExtractPlugin({ filename: "style.css" });
   return {
     mode: process.env.NODE_ENV,
-    entry: path.resolve(__dirname, "src/app.js"),
-    // entry: path.resolve(__dirname, "src/playground/HOC.js"),
+    entry: ["babel-polyfill", path.resolve(__dirname, "src/app.js")],
     output: {
       path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js"
@@ -52,14 +51,30 @@ module.exports = env => {
     plugins: [
       MiniCssExtract,
       new webpack.DefinePlugin({
-        "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
-        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-        "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID),
-        "process.env.FIREBASE_MEASUREMENT_ID": JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
+        "process.env.FIREBASE_API_KEY": JSON.stringify(
+          process.env.FIREBASE_API_KEY
+        ),
+        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+          process.env.FIREBASE_AUTH_DOMAIN
+        ),
+        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+          process.env.FIREBASE_DATABASE_URL
+        ),
+        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
+        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        ),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID
+        ),
+        "process.env.FIREBASE_APP_ID": JSON.stringify(
+          process.env.FIREBASE_APP_ID
+        ),
+        "process.env.FIREBASE_MEASUREMENT_ID": JSON.stringify(
+          process.env.FIREBASE_MEASUREMENT_ID
+        )
       })
     ],
     devtool: isProduction ? "source-map" : "inline-source-map",
